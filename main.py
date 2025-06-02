e3d7dcabfed0133dbca9a52d9cdbfb15fc51713a
CuttleC <cuttlecraft.ww@gmail.com>
2025-06-02 16:19:43 -0700

    wrote functionality to count words in source book

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    if file_contents != None:
+        return file_contents
+    return "no book found"
+
+def word_counter(book_text):
+    book_words = book_text.split()
+    return len(book_words)
+
+def main():
+    book_contents = get_book_text("books/frankenstein.txt")
+    amount_of_words = word_counter(book_contents)
+    print(f"{amount_of_words} words found in the document")
+
+main()
