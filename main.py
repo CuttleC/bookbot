ab525acdc9a80668be7532b9cde5c52289f04833
CuttleC <cuttlecraft.ww@gmail.com>
2025-06-02 19:00:15 -0700

    the text gets added to a dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import word_counter, char_counter
 
 def get_book_text(file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -3,8 +3,13 @@ def word_counter(book_text):
     return len(book_words)
 
 def char_counter(book_text):
-    # for each char in book_text
-    # check if it's in our dictionary already
-    # if so, increase its count by 1
-    # otherwise add it to the dictionary with the number 1
-    # return the dictionary
+    all_chars = {}
+    
+    for character in book_text:
+        lower_character = character.lower()
+        if lower_character in all_chars:
+            all_chars[lower_character] += 1
+        else:
+            all_chars[lower_character] = 1
+    
+    return all_chars
